03e8ea87d828548bf094778bcf74279d371939a8
Chayan Pal <chayan110201@gmail.com>
2024-08-10 21:15:08 +0530

    Function to generate random numbers in Second Activity added

---
+++ app/src/main/java/com/example/luckynumberapp/SecondActivity.java
@@ -0,0 +1,51 @@
+package com.example.luckynumberapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
+
+public class SecondActivity extends AppCompatActivity {
+
+    TextView welcomeTxt, luckyNumberTxt;
+    Button share_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_second);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        welcomeTxt = findViewById(R.id.textView2);
+        luckyNumberTxt = findViewById(R.id.lucky_number_txt);
+        share_btn = findViewById(R.id.share_btn);
+
+        //Receiving the data from Main Activity
+        Intent i = getIntent();
+        String userName = i.getStringExtra("name");
+
+        //Generating Random Numbers
+        int random_num = generateRandomNumber();
+        luckyNumberTxt.setText(""+random_num);
+    }
+
+    public int generateRandomNumber() {
+        Random random = new Random();
+        int upper_limit = 1000;
+        int randomNumberGenerated = random.nextInt(upper_limit);
+        return randomNumberGenerated;
+    }
+}
