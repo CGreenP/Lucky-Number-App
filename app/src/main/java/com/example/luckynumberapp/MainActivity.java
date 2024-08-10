61528823fe788f8206eba41ba1ac4cd6d3d6f6a6
Chayan Pal <chayan110201@gmail.com>
2024-08-10 21:11:40 +0530

    Function to pass data to Second Activity added

--- app/src/main/java/com/example/luckynumberapp/MainActivity.java
+++ app/src/main/java/com/example/luckynumberapp/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.luckynumberapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +15,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button btn;
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +29,27 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        txt = findViewById(R.id.textView);
+        editText = findViewById(R.id.edit_text);
+        btn = findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName = editText.getText().toString();
+                if (userName.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please give valid input!", Toast.LENGTH_SHORT).show();
+                } else {
+                    //Explicit Intent
+                    Intent i = new Intent(getApplicationContext(),SecondActivity.class);
+
+                    //Passing the name to second activity
+                    i.putExtra("name", userName);
+
+                    startActivity(i);}
+            }
         });
     }
 }
